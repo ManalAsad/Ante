394bd0b6daf866b2b76738b646592f0d29134a95
Nikita Sharma <sharm046@csusm.edu>
2026-09-19 20:25:37 -0700

    Fix index.js to only export goal logic Refs #3

--- logic/index.js
+++ logic/index.js
@@ -1,15 +1,10 @@
 const goalLogic = require('./goalLogic');
-const budgetLogic = require('./budgetLogic');
-const themes = require('./themes');
-const { ValidationError, FREQUENCIES, BUDGET_PERIODS } = require('./validation');
+const { ValidationError, FREQUENCIES } = require('./validation');
 const { DATA_FILE } = require('./dataStore');
 
 module.exports = {
   ...goalLogic,
-  ...budgetLogic,
-  ...themes,
   ValidationError,
   FREQUENCIES,
-  BUDGET_PERIODS,
   DATA_FILE,
 };
